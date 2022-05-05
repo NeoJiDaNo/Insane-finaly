60862e2b221a55f1bf27b371a914ecae1485eba1
Vasily <vasily-bazhenov.03@mail.ru>
2022-05-05 13:49:15 +0300

    add module(plansRepairSlaider)

--- src/modules/plansRepairSlaider.js
+++ src/modules/plansRepairSlaider.js
@@ -1,44 +1,73 @@
 const repairSlaider = () => {
     const navList = document.querySelector('.nav-list-repair')
     const repairNavs = document.querySelectorAll('.repair-types-nav__item')
-    const arrowRight = document.querySelector('#repair-types-arrow_right')
-    const arrowLeft = document.querySelector('#repair-types-arrow_left')
-    let count = 0
+    const slideCount = document.querySelector('.slider-counter-content__current')
+    const slideTotal = document.querySelector('.slider-counter-content__total')
+    const repairSlides1 = document.querySelectorAll('.types-repair1 > .repair-types-slider__slide')
+    const repairSlides2 = document.querySelectorAll('.types-repair2 > .repair-types-slider__slide')
+    const repairSlides3 = document.querySelectorAll('.types-repair3 > .repair-types-slider__slide')
+    const repairSlides4 = document.querySelectorAll('.types-repair4 > .repair-types-slider__slide')
+    const repairSlides5 = document.querySelectorAll('.types-repair5 > .repair-types-slider__slide')
+    let numSlides = repairSlides1
+    let count = 1
+    let indexSlaid = 0
+    slideTotal.textContent = numSlides.length
 
-    const prevSlide = (index, strClass) => {
-        repairNavs[index].classList.remove(strClass)
-    }
-
-    const nextSlide = (index, strClass) => {
-        repairNavs[index].classList.add(strClass)
-    }
-
-    repairNavs.forEach((btn) => {
+    repairNavs.forEach((btn, key) => {
         btn.addEventListener('click', (e) => {
             e.preventDefault()
             const elem = navList.querySelector('.active')
             elem.classList.remove('active')
             e.target.classList.add('active')
+            slideOff()
+            console.log(numSlides);
+            if (key == 0) {numSlides = repairSlides1} else if (key == 1) {numSlides = repairSlides2} else if (key == 2) {numSlides = repairSlides3} else if (key == 3) {numSlides = repairSlides4} else if (key == 4) {numSlides = repairSlides5}
+            slideTotal.textContent = numSlides.length
+            console.log(numSlides);
+            slideOn()
+            slideCount.textContent = 1
+            count = 1
         })
     })
-    
-    arrowRight.addEventListener('click', () => {
-        prevSlide(count, 'active')
-        count++
-        if (count == 5) {
-            count = 0
-        }
-        nextSlide(count, 'active')
-    })
 
-    arrowLeft.addEventListener('click', () => {
-        prevSlide(count, 'active')
-        count--
-        if (count == 5) {
-            count = 0
+    const slideOff = () => {
+        numSlides.forEach((btn, key) => {
+            numSlides[key].style.display = 'none'
+        })
+    }
+
+    const slideOn = () => {
+        numSlides[0].style.display = 'block'
+    }
+
+    document.addEventListener('click', (e) => {
+        if (e.target.closest('#repair-types-arrow_right')) {
+            if (count == slideTotal.textContent) {
+                count = 0
+            }
+            count++
+            slideCount.textContent = count
+            numSlides[indexSlaid].style.display = 'none'
+            indexSlaid++
+            if (indexSlaid == numSlides.length - 1) {
+                indexSlaid = 0
+            }
+            numSlides[indexSlaid].style.display = 'block'
         }
-        nextSlide(count, 'active')
-        
+
+        if (e.target.closest('#repair-types-arrow_left')) {
+            count--
+            if (count == 0) {
+                count = slideTotal.textContent
+            }
+            slideCount.textContent = count
+            numSlides[indexSlaid].style.display = 'none'
+            if (indexSlaid == 0) {
+                indexSlaid = numSlides.length - 1
+            }
+            indexSlaid--
+            numSlides[indexSlaid].style.display = 'block'
+        }
     })
 }
 
